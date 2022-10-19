52adbdffbfd096ccefac7b8640e171360a847f0d
Mayfield Mayfield <mayfieldmel@gmail.com>
2022-10-19 14:05:17 -0400

    connect front & backend & concurrently

--- client/src/App.js
+++ client/src/App.js
@@ -1,19 +1,33 @@
 import React from 'react';
+import { ApolloProvider, ApolloClient, InMemoryCache, createHttpLink } from '@apollo/client';
 
 import Header from './components/Header';
 import Footer from './components/Footer';
 
 import Home from './pages/Home';
 
+// first, establish a new link to the GraphQL server at its /graphql endpoint
+const httpLink = createHttpLink({
+  uri: '/graphql',
+});
+// second, instantiate the Apollo Client instance and create the connection to the API endpoint.
+const client = new ApolloClient({
+  link: httpLink,
+  // instantiate a new cache object
+  cache: new InMemoryCache(),
+});
+
 function App() {
   return (
-    <div className='flex-column justify-flex-start min-100-vh'>
-      <Header />
-      <div className='container'>
-        <Home />
+    <ApolloProvider client={client}>
+      <div className="flex-column justify-flex-start min-100-vh">
+        <Header />
+        <div className="container">
+          <Home />
+        </div>
+        <Footer />
       </div>
-      <Footer />
-    </div>
+    </ApolloProvider>
   );
 }
 
